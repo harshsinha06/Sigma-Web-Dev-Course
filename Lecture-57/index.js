ecac239829aeb6c5a013813ef79481c1f939338f
Harsh Sinha <harshrazz600@gmail.com>
2024-06-26 13:26:20 +0530

    Lecture - 57 completed

---
+++ Lecture-57/index.js
@@ -0,0 +1,30 @@
+console.log("I am a tutorial on Loops.");
+
+// let a = 1;
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+
+// let obj = {name : "Harsh", role : "Programmer", company: "Jane Street", salary : "2.4 Crore per anum"}
+
+// for (const key in obj) {
+//     // const element = obj[key];
+//     console.log(key)
+// }
+
+// for (const c of "Harry") {
+//     console.log(c);
+// }
+
+// let i = 0;
+// while (i<6000) {
+//     console.log(i);
+//     i++;
+// }
+
+let i = 6;
+do {
+    console.log(i)
+    i++;
+} while (i<6)
