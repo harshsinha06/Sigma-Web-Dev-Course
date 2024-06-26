a77713a3b27b6b5ae27b3a0d4b4eef365ab498c9
Harsh Sinha <harshrazz600@gmail.com>
2024-06-26 13:25:51 +0530

    Lecture-55 completed

---
+++ Lecture-55/practice.js
@@ -0,0 +1,36 @@
+// Q-1
+
+// let a = "Harsh";
+// let b = 1;
+// console.log(a+b);
+
+
+
+// Q-2
+
+// let a = "Harsh";
+// console.log(typeof a);
+
+
+
+// Q-3
+
+// const obj = {name : "Harsh", age : 20};
+// console.log(obj);
+
+// // obj = 6;
+// // console.log(obj);
+
+
+
+// Q-5
+
+const dict = {
+    Esoteric: "Understood by only a few with special knowledge.",
+    Ubiquitous: "Found everywhere.",
+    Quintessential: "Perfect example of something.",
+    Obfuscate: "Make unclear or confusing.",
+    Nonplussed: "Surprised and confused.",
+}
+
+console.log(dict);
---
+++ Lecture-55/script.js
@@ -0,0 +1,29 @@
+console.log("Hey this is Tutorial 55 of Sigma Web Development")
+
+var a = 5;
+var b = 6;
+var c = "Harry"
+console.log(a + b + 8)
+console.log(typeof a, typeof b, typeof c)
+
+const a1 = 6;
+
+let x = "Harry Bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    name : "Harry",
+    "job role" : 5600,
+    "is_handsome": true
+}
+console.log(o)
+
+o.salary = "50 LPA"
+console.log(o)
