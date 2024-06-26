a661491a49c1aa170e7dc239b504a2c2d94cf185
Harsh Sinha <harshrazz600@gmail.com>
2024-06-26 13:26:09 +0530

    Lecture-56 completed

---
+++ Lecture-56/practice.js
@@ -0,0 +1,22 @@
+// Q-1
+
+// var age = 12
+// if(age > 10 && age < 20){
+//     console.log("You are teenager")
+// }
+
+
+
+
+// Q - 3 & 4
+
+let num = 9;
+ if(num % 2 == 0 && num % 3 != 0){
+    console.log("number is only divisible by 2");
+ }
+
+ if(num % 3 == 0 && num % 2 != 0){
+    console.log("number is only divisible by 3");
+ }
+
+ 
