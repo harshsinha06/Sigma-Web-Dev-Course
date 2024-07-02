c7b0a224ccedb0bf8976b2d8998cb8934ac9de79
Harsh Sinha <harshrazz600@gmail.com>
2024-07-02 19:39:58 +0530

    Exercise-11 completed

---
+++ Lecture-65 (Exericse-11)/script.js
@@ -0,0 +1,32 @@
+/* Write a program to calculate factorial of a number using reduce and using for loops
+
+6! = 6*5*4*3*2*1
+
+*/
+
+let n = parseInt(prompt("Enter a number to find factorial :"));
+console.log(n);
+
+
+// let fact = 1;
+// for (let i = 1; i <= n; i++) {
+//     fact *= i;
+// }
+// alert(`Factorial of ${n} is ${fact}`);
+
+
+
+// let arr = [];
+// let fact = 1;
+
+// for(let i = 0; i < n; i++){
+//     arr[i] = i+1;
+// }
+
+// const fact_function = (a, b) => a*b;
+
+// fact = arr.reduce(fact_function);
+// alert(`Factorial of ${n} is ${fact}`);
+
+
+
