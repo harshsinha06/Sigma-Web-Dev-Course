1ae02819f5381633134e17ff4142717acc32de92
Harsh Sinha <harshrazz600@gmail.com>
2024-08-13 13:16:19 +0530

    Lecture-86 completed

---
+++ Lecture-86/main.js
@@ -0,0 +1,14 @@
+// import {a, b, d} from './my_modules.js';
+// console.log(a, b, d);
+
+// import harry from "./my_module.js";
+// console.log (harry);
+
+(function(exports, require, module, __filename, __dirname){
+
+  // Module code actually lives here
+
+});
+
+const a = require("./my_module2.js");
+console.log(a, __dirname, __filename);
